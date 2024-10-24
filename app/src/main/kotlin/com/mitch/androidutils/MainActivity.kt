0282e7594c87d12312b56a433c71946d0e3c4c9a
Andrea Severi <andrea.severi.dev@gmail.com>
2024-10-24 09:33:16 +0200

    change main activity to reflect template

--- app/src/main/kotlin/com/mitch/androidutils/MainActivity.kt
+++ app/src/main/kotlin/com/mitch/androidutils/MainActivity.kt
@@ -1,43 +1,94 @@
 package com.mitch.androidutils
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.mitch.androidutils.ui.theme.AndroidUtilsTheme
+import com.mitch.androidutils.utils.designsystem.components.sheets.bottom.AppModalBottomSheet
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
+            val isThemeDark = isSystemInDarkTheme()
+            DisposableEffect(isThemeDark) {
+                enableEdgeToEdge(
+                    statusBarStyle = SystemBarStyle.auto(
+                        Color.TRANSPARENT,
+                        Color.TRANSPARENT
+                    ) { isThemeDark },
+                    navigationBarStyle = SystemBarStyle.auto(
+                        LightScrim,
+                        DarkScrim
+                    ) { isThemeDark }
+                )
+                onDispose { }
+            }
+
             AndroidUtilsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                var showDialog by rememberSaveable { mutableStateOf(false) }
+                Scaffold(contentWindowInsets = WindowInsets(0)) { padding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(padding)
+                            .consumeWindowInsets(padding)
+                            .windowInsetsPadding(
+                                WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal)
+                            )
+                    ) {
+                        Button(
+                            onClick = { showDialog = true },
+                            modifier = Modifier.align(Alignment.Center)
+                        ) {
+                            Text("Show")
+                        }
+                        if (showDialog) {
+                            AppModalBottomSheet(onDismiss = { showDialog = false }) {
+                                Button(onClick = { showDialog = false }) {
+                                    Text("Chiudi")
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+/**
+ * The default light scrim, as defined by androidx and the platform:
+ * https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:activity/activity/src/main/java/androidx/activity/EdgeToEdge.kt;l=35-38;drc=27e7d52e8604a080133e8b842db10c89b4482598
+ */
+private val LightScrim = Color.argb(0xe6, 0xFF, 0xFF, 0xFF)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidUtilsTheme {
-        Greeting("Android")
-    }
-}
+/**
+ * The default dark scrim, as defined by androidx and the platform:
+ * https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:activity/activity/src/main/java/androidx/activity/EdgeToEdge.kt;l=40-44;drc=27e7d52e8604a080133e8b842db10c89b4482598
+ */
+private val DarkScrim = Color.argb(0x80, 0x1b, 0x1b, 0x1b)
